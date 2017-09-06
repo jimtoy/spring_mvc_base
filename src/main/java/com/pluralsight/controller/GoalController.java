dc67852d540a8852f896b2684ca5d10d60ac69ef
jim <jim.toy@gmail.com>
2017-09-05 23:12:24 -0400

    got transactional to work

--- src/main/java/com/pluralsight/controller/GoalController.java
+++ src/main/java/com/pluralsight/controller/GoalController.java
@@ -1,7 +1,8 @@
 package com.pluralsight.controller;
 
-import javax.validation.Valid;
-
+import com.pluralsight.model.Goal;
+import com.pluralsight.service.GoalService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -10,33 +11,39 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
-import com.pluralsight.model.Goal;
+import javax.validation.Valid;
 
 @Controller
 @SessionAttributes("goal")
 public class GoalController {
 
-	@RequestMapping(value = "addGoal", method = RequestMethod.GET)
-	public String addGoal(Model model) {
-		Goal goal = new Goal();
-		goal.setMinutes(10);
-		model.addAttribute("goal", goal);
-		
-		return "addGoal";
-	}
-	
-	@RequestMapping(value = "addGoal", method = RequestMethod.POST)
-	public String updateGoal(@Valid @ModelAttribute("goal") Goal goal, BindingResult result) {
-		
-		System.out.println("result has errors: " + result.hasErrors());
-		
-		System.out.println("Goal set: " + goal.getMinutes());
-		
-		if(result.hasErrors()) {
-			return "addGoal";
-		}
-		
-		return "redirect:index.jsp";
-	}
-	
+    @Autowired
+    private GoalService goalService;
+
+    @RequestMapping(value = "addGoal", method = RequestMethod.GET)
+    public String addGoal(Model model) {
+        Goal goal = new Goal();
+        goal.setMinutes(10);
+        model.addAttribute("goal", goal);
+
+        return "addGoal";
+    }
+
+    @RequestMapping(value = "addGoal", method = RequestMethod.POST)
+    public String updateGoal(@Valid @ModelAttribute("goal") Goal goal, BindingResult result) {
+
+        System.out.println("result has errors: " + result.hasErrors());
+
+        System.out.println("Goal set: " + goal.getMinutes());
+
+        if (result.hasErrors()) {
+            return "addGoal";
+        } else {
+            goalService.save(goal);
+        }
+
+
+        return "redirect:index.jsp";
+    }
+
 }
